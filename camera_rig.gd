d0f984cd3d96e52ca94ff0cbd8fed1c11addb705
Gary <thebigbeef13@gmail.com>
2026-08-01 10:40:29 -0400

    add Camera-Relative Movement, Mouse-Look Camera Rig, Set Main Scene

---
+++ camera_rig.gd
@@ -0,0 +1,23 @@
+extends Node3D
+@onready var spring_arm_3d: SpringArm3D = $SpringArm3D
+
+@export var player: CharacterBody3D
+
+var yaw: float = 0.0
+var pitch: float = 0.0
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * 0.003
+		pitch -= event.relative.y * 0.003
+func _process(_delta: float) -> void:
+	pitch = clamp(pitch, deg_to_rad(-40.0), deg_to_rad(70.0))
+	rotation.y = yaw
+	spring_arm_3d.rotation.x = pitch
+func _physics_process(delta: float) -> void:
+	if not player:
+		return
+	global_position = global_position.lerp(player.global_position, 12.0 * delta)
+	
--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@export var camera_rig: Node3D
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -17,7 +19,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (camera_rig.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
