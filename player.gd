8f333bc47cadcf32bca12e22f4071c52256687fb
Gary <thebigbeef13@gmail.com>
2026-08-02 07:29:15 -0400

    added double jump

--- player.gd
+++ player.gd
@@ -2,23 +2,24 @@ extends CharacterBody3D
 
 @export var camera_rig: Node3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const TURN_SPEED = 10.0
+const MAX_JUMPS = 2
 
+var jump_count = 2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump and double jump
+	if is_on_floor():
+		jump_count = MAX_JUMPS
+	if Input.is_action_just_pressed("jump") and jump_count > 0:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count -= 1
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (camera_rig.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -29,5 +30,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
